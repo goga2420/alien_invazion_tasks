caa44b6890d978ba14e62f5bb69cec1733aac3fb
goga2420 <58983503+goga2420@users.noreply.github.com>
2020-01-19 22:12:02 +0300

    Корабль должен двигаться во все стороны и стрелять в бок вправо

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 255)
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0, 255)
